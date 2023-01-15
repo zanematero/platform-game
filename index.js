b20747ac2d5212ab33cb71cd55db1254c25a3a73
Zane <114646680+zanematero@users.noreply.github.com>
2023-01-15 14:33:11 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// move player icon
+// select player icon as moveable character
+// directional arrow movement
+/* 
+arrow up - jump
+arrow left - left
+arrow right - right
+*/
+
+// ground/jump mechanics?
+// if ground object then not able to move through
+// some logic to gravity after jumping
+/* grid system jump - +3
+automatic -1 every second */
+
+// spike logic
+// if player intersects with spike then damage taken
+// some sort of visual effect to show damage has been taken
+
+// update health bar
+// if damage taken then subtract health point
+
+// death event
+// if no health points then activate dead screen
+// death screen includes message and replay option also include current score
+
+// collect star
+// if user intersects star then remove star and increase score count
+
+// when all star(s) collected
+// win event including replay option
+// include score and possibly time?
+
+// reset button, resets time, position, health, score, stars
+// track time, begins on load
