545305e8916ef758ac24be58b9b5c6ec61271961
Zane Matero <materozane@gmail.com>
2023-01-17 21:43:25 -0800

    bring to current

--- index.js
+++ index.js
@@ -1,31 +1,148 @@
-// move player icon
-// select player icon as moveable character
-// directional arrow movement
-/* 
-arrow up - jump
-arrow left - left
-arrow right - right
-*/
+// add player icon
 
-// ground/jump mechanics?
-// if ground object then not able to move through
-// some logic to gravity after jumping
-/* grid system jump - +3
-automatic -1 every second */
+function newImage(url){
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'absolute'
+    document.body.append(image)
+    return image
+}
+
+// select player icon as moveable character
+
+function playableCharacter(x, y) {
+    const element = newImage('assets/stand.png')
+    element.style.zIndex = 1;
+
+    function handleDirectionChange(direction) {
+        if (direction === null) {
+            element.src = `assets/stand.png`
+        }
+        if (direction === 'west') {
+            element.src = `assets/run_back.png`
+        }
+        if (direction === 'north') {
+            element.src = `assets/jump.png`
+        }
+        if (direction === 'east') {
+            element.src = `assets/run.png`
+        }
+        if (direction === 'northeast') {
+            element.src = `assets/jump.png`
+        }
+        if (direction === 'northwest') {
+            element.src = `assets/jump_back.png`
+        }
+    }
+
+    move(element).withArrowKeys(x, y, handleDirectionChange)
+
+    return {
+        element: element
+    }
+}
+
+// directional arrow movement
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            /* if(direction === 'northeast'){
+                x+=1
+                y+=1
+            }
+            if(direction === 'northwest'){
+                x-=1
+                y+=1
+            } */
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            /* if(e.key === 'ArrowRight' && e.key === 'ArrowUp'){
+                direction = 'northeast'
+            }
+            if(e.key === 'ArrowLeft' && e.key === 'ArrowUp'){
+                direction = 'northwest'
+            } */
+            callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys,
+    }
+}
+
+/* function gravity(element) {
+    let y = bottom;
+    element.style.bottom = fall()
+    function fall(){
+        setInterval((y -= 1), 1)
+    }
+    element.style.bottom = fall + 'px'
+}
+
+gravity(playableCharacter,0) */
+
+const pc = playableCharacter(10,130)
 
 // spike logic
+
+// const spike = 
 // if player intersects with spike then damage taken
 // some sort of visual effect to show damage has been taken
 
-// update health bar
-// if damage taken then subtract health point
-
 // death event
-// if no health points then activate dead screen
 // death screen includes message and replay option also include current score
 
-// collect star
 // if user intersects star then remove star and increase score count
+
+move(newImage('assets/turkey.png')).to(450, 140)
+move(newImage('assets/spike.png')).to(200, 140)
 
 // when all star(s) collected
 // win event including replay option
