0cfe0d8ea17259830d19ed591aef5544a8de12e0
Zane Matero <materozane@gmail.com>
2023-01-26 17:58:29 -0800

    add spike

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Ability to reset
 var button = document.getElementById('refreshButton');
-document.getElementById('body').onload = function(){
+body.onload = function() {
     console.log('DOM loaded')
-    document.getElementById('refreshButton').onclick = function(){
+    button.onclick = function() {
         window.location.reload()
         console.log('Page reloaded')
     }
-    // button.addEventListener('onclick', refresh);
-};
+}
 
 // Function to add player image
 function newPlayer(url){
@@ -15,39 +14,29 @@ function newPlayer(url){
     image.src = url;
     image.style.position = 'absolute';
     return image;
+};
+
+// Add spike image to canvas
+function spike(x,y){
+    if(canvas.getContext){
+        var img = new Image();
+        img.src = "assets/spike.png";
+        img.onload = function(){
+        ctx.drawImage(img,x,y)
+        }
+    }
 }
-
-// Function to add reset and score elements
-/* function move(url) {
-    let image = document.createElement('img');
-    image.src = url;
-    element.style.position = 'absolute';
-    document.body.append(image)
-    return image
-    
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-        document.getElementsByTagName('img').style.zIndex = "1";
-    }
-
-    return {
-        to: moveToCoordinates
-    }
-} */
-
-/* move(newImage('assets/turkey.png')).to(450, 140)
-move(newImage('assets/spike.png')).to(200, 140) */
 
 // Canvas creation
 function createCanvas() {
     ctx.fillStyle = "#f2d0bf";
     ctx.fillRect(0, 0, 1040, 560);
-}
+};
+
 cWidth = 1040;
 cHeight = 560;
 canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 ctx.canvas.height = cHeight;
 ctx.canvas.width = cWidth;
 
@@ -69,29 +58,29 @@ var animation = {
     left: "assets/run_back.png",
     right: "assets/run.png",
     stand: "assets/stand.png"
-}
+};
 
 if (playerInfo.xVelocity > -1 && playerInfo.xVelocity < 1) {
     stanceCurrent = animation.stand;
-}
+};
 
 // Assigning canvas element for player to detect collision
 function createPlayer() {
     ctx.fillStyle = "#F08080";
     img = newPlayer(stanceCurrent);
     ctx.drawImage(img, (playerInfo.x + 35) - playerInfo.width, (playerInfo.y) - playerInfo.height);
-}
+};
 
 // Movement
 var keysCurrent = {
     right: false,
     left: false,
     jump: false
-}
+};
 
-var movementGravity = 0.5;
+var movementGravity = 0.25;
 
-var movementFriction = 0.5;
+var movementFriction = 0.1;
 
 function keydown(e) {
     // Left arrow
@@ -102,7 +91,7 @@ function keydown(e) {
     // Up arrow
     if (e.keyCode == 38) {
         if (playerInfo.jumping == false) {
-            playerInfo.yVelocity = -7.5;
+            playerInfo.yVelocity = -5;
             console.log('jump')
         }
     }
@@ -128,10 +117,10 @@ function keyup(e){
         keysCurrent.right = false;
         console.log('right')
     }
-}
+};
 
 // Platform creation
-var platforms = []
+var platforms = [];
 var platformCount = 5;
 
 function renderPlatform() {
@@ -139,14 +128,14 @@ function renderPlatform() {
     for (i = 0; i < platforms.length; i++) {
         ctx.fillRect(platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height);
     }
-}
+};
 
 // Platform location assignment
 function createPlatform() {
     for (i = 0; i < platformCount; i++) {
         platforms.push(
             {
-                x: 2.25 * (i * 400),
+                x: 5 * (i * 400),
                 y: 200,
                 width: 100,
                 height: 10
@@ -164,14 +153,20 @@ function createPlatform() {
                 height: 10
             },
             {
-                x: 2.25 * (i * 325),
+                x: 5 * (i * 325),
                 y: 300,
                 width: 250,
                 height: 10
             },
+            {
+                x: 2 * (i + 232.5),
+                y: 225,
+                width: 75,
+                height: 10
+            },
         );
-    }
-}
+    };
+};
 
 // Logic to check and apply movement/collision
 function checkStatus() {
@@ -184,11 +179,11 @@ function checkStatus() {
     playerInfo.jumping = true;
 
     if (keysCurrent.left) {
-        playerInfo.xVelocity = -5;
+        playerInfo.xVelocity = -3;
         stanceCurrent = animation.left;
     }
     if (keysCurrent.right) {
-        playerInfo.xVelocity = 5;
+        playerInfo.xVelocity = 3;
         stanceCurrent = animation.right;
     }
     playerInfo.y += playerInfo.yVelocity;
@@ -213,144 +208,17 @@ function checkStatus() {
     createCanvas();
     createPlayer();
     renderPlatform();
+    spike(10,250);
 }
 createPlatform();
 setInterval(checkStatus, 25);
 document.addEventListener('keydown', keydown);
 document.addEventListener('keyup', keyup);
 
-
-
-
-// Original movement mechanic
-/* function main(x, y) {
-    const element = newImage('assets/stand.png')
-    element.style.zIndex = 1;
-
-    function handleDirectionChange(direction) {
-        if (direction === null) {
-            element.src = `assets/stand.png`
-        }
-        if (direction === 'left') {
-            element.src = `assets/run_back.png`
-        }
-        if (direction === 'jump') {
-            element.src = `assets/jump.png`
-        }
-        if (direction === 'right') {
-            element.src = `assets/run.png`
-        }
-        if (direction === 'northeast') {
-            element.src = `assets/jump.png`
-        }
-        if (direction === 'northwest') {
-            element.src = `assets/jump_back.png`
-        }
-    }
-
-    move(element).withArrowKeys(x, y, handleDirectionChange)
-
-    return {
-        element: element
-    }
-} */
-
-    /* function moveWithArrowKeys(left, bottom, callback){
-        let direction = null;
-        let x = left;
-        let y = bottom;
-
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
-        
-        function moveCharacter(){ 
-            if(direction === 'left'){
-                x-=1
-            }
-            if(direction === 'jump'){
-                y+=1
-            }
-            if(direction === 'right'){
-                x+=1
-            }
-            if(direction === 'northeast'){
-                x+=1
-                y+=1
-            }
-            if(direction === 'northwest'){
-                x-=1
-                y+=1
-            }
-            element.style.left = x + 'px'
-            element.style.bottom = y + 'px'
-
-            //track if character move off screen
-            if(element.style.left < -75 + 'px'){
-                reset()
-            }
-        }
-        
-        setInterval(moveCharacter, 1)
-        
-        document.addEventListener('keydown', function(e){
-            if(e.repeat) return;
-        
-            if(e.key === 'ArrowLeft'){
-                direction = 'left'
-            }
-            if(e.key === 'ArrowUp'){
-                direction = 'jump'
-            }
-            if(e.key === 'ArrowRight'){
-                direction = 'right'
-            }
-            if(e.key === 'ArrowRight' && e.key === 'ArrowUp'){
-                direction = 'northeast'
-            }
-            if(e.key === 'ArrowLeft' && e.key === 'ArrowUp'){
-                direction = 'northwest'
-            }
-            callback(direction)
-        })
-        
-        document.addEventListener('keyup', function(e){
-            direction = null
-            callback(direction)
-        })
-    } */
-
-/* function gravity(element) {
-    let y = bottom;
-    function fall(){
-        (y -= 1)
-    }
-    element.style.bottom = fall + 'px'
-    setInterval(fall, 1)
-}
-
-gravity(main) */
-
-//create main character
-/* function spawn(character,x,y) {
-const pc = character(x,y)
-}
-addEventListener(onload,spawn(main,10,130)) */
-
-//ability to reset from start if death/move off screen
-/* function reset() {
-    delete(main.newImage)
-    move(main.newImage).to(10, 130)
-} */
-
 // spike logic
 
 // const spike = 
 // if player intersects with element, player is reset
-
-//platform element
-
-// death event
-// death screen includes message and replay option also include current score
 
 // if user intersects star then remove star and increase score count
 
